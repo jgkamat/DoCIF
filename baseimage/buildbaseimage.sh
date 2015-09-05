664d8a3b524d21b8af07e224c9598354244cc743
Jay Kamat <github@jgkamat.33mail.com>
2015-09-05 17:50:16 -0400

    Refactor docif variable names some more

--- baseimage/buildbaseimage.sh
+++ baseimage/buildbaseimage.sh
@@ -5,23 +5,30 @@ set -e
 DIR=$(cd $(dirname $0) ; pwd -P)
 source ${DIR}/../docker_common.sh
 
-if curl -s https://registry.hub.docker.com/v1/repositories/${IMAGE_NAME_BASE}/tags |  fgrep -q "\"name\": \"${SHA_SUM_SETUP}\""; then
+if [ -n "$CACHING_SHA" ] && curl -s https://registry.hub.docker.com/v1/repositories/${BASEIMAGE_REPO}/tags |  fgrep -q "\"name\": \"${CACHING_SHA}\""; then
 	# The tag currently exists, and can be pulled
-	docker pull ${IMAGE_NAME_BASE}:${SHA_SUM_SETUP}
+	docker pull ${BASEIMAGE_REPO}:${CACHING_SHA}
 else
 	# The tag does not exist, let's build it!
-	docker build -t ${IMAGE_NAME_BASE}:in_progress  -f ${ROBOCUP_ROOT}/util/docker/baseimage/Dockerfile .
+	docker build -t ${BASEIMAGE_REPO}:in_progress  -f ${ROBOCUP_ROOT}/util/docker/baseimage/Dockerfile .
 	docker run \
 		   --entrypoint="/bin/bash" \
-		   -v ${ROBOCUP_ROOT}:/home/developer/robocup-software ${IMAGE_NAME_BASE}:in_progress \
+		   -v ${ROBOCUP_ROOT}:/home/developer/robocup-software ${BASEIMAGE_REPO}:in_progress \
 		   -c './util/ubuntu-setup --yes --firmware && sudo apt-get clean'
 
-	docker commit "$(docker ps -aq | head -n1)" ${IMAGE_NAME_BASE}:${SHA_SUM_SETUP}
+	docker commit "$(docker ps -aq | head -n1)" ${BASEIMAGE_REPO}:${CACHING_SHA:-latest}
 
-	if [ -n "$DOCKER_PASS" ]; then
+	if [ -n "$DOCKER_PASSWORD" -a -n "$DOCKER_EMAIL" -a -n "$DOCKER_USERNAME" ]; then
 		docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-		docker push ${IMAGE_NAME_BASE}:${SHA_SUM_SETUP} #
+		# Exit on failed login
+		if [ $? -ne 0 ]; then
+			echo "[WARN] Docker login FAILED. This most likeley means invalid credentials Exiting..." >&2
+			exit 1
+		fi
+		docker push ${BASEIMAGE_REPO}:${CACHING_SHA:-latest}
+	else
+		echo "[WARN] Docker credentials not found. Skipping push." >&2
 	fi
 fi
 
-docker tag -f ${IMAGE_NAME_BASE}:${SHA_SUM_SETUP} ${IMAGE_NAME_BASE}:current
+docker tag -f ${BASEIMAGE_REPO}:${CACHING_SHA:-latest} ${BASEIMAGE_REPO}:current
