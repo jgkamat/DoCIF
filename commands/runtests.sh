80eed419a75be3258a6f900a6a14218275f121a4
Jay Kamat <github@jgkamat.33mail.com>
2015-11-29 18:23:06 -0500

    Use trap to always wait for push

--- commands/runtests.sh
+++ commands/runtests.sh
@@ -10,7 +10,40 @@ if [ "$1" = "--pending" ]; then
 	TEST_CMD="${TEST_CMD} --pending"
 fi
 
-# Entrypiont is needed to preserve exit code
+# Wait for push to finish if possible
+wait_push() {
+	if [ -r "${TMP_FOLDER}/push_baseimage.lock" ]; then
+		echo "[INFO] Waiting for the baseimage push to complete."
+
+		set +e
+
+		COUNTER=0
+		TIMEOUT=600
+		# Wait for the push to baseimage to finish
+		if [ -f ${TMP_FOLDER}/push_baseimage.lock ]; then
+			while [ -z "$(cat ${TMP_FOLDER}/push_baseimage.lock)" ]; do
+				sleep 1
+				COUNTER="$(expr "$COUNTER" + 1)"
+				if [ "$COUNTER" -gt "$TIMEOUT" ]; then
+					# Break out of loop by forcing a failure
+					echo "[ERR] Push command timed out!" >&2
+					echo "1" > "${TMP_FOLDER}/push_baseimage.lock"
+				fi
+			done
+		fi
+
+		if [ "$(cat ${TMP_FOLDER}/push_baseimage.lock)" -ne 0 ]; then
+			echo "[WARN] The push to the source repository FAILED. This usually means the dockerhub is down." >&2
+		fi
+		set -e
+
+		rm -f ${TMP_FOLDER}/push_baseimage.lock
+	fi
+}
+
+trap "wait_push" EXIT
+
+
 docker run \
 	--cidfile="${TMP_FOLDER}/docif_tests.cid" \
 	$(eval echo \"$(${DIR}/../util/docker_common.sh print_cache_flags)\") \
@@ -25,9 +58,7 @@ docker run \
 
 
 EXIT=$?
-if [ $EXIT -ne 0 ]; then
-	exit $EXIT
-fi
+
 
 if [ ! -f "${TMP_FOLDER}/docif_tests.cid" ]; then
 	echo "[ERR] Your tests command did not produce a usable container. This is likely a config or infra issue." >&2
@@ -38,31 +69,8 @@ fi
 docker commit "$(cat ${TMP_FOLDER}/docif_tests.cid)" ${IMAGE_NAME_CI}:${COMMIT_SHA}
 rm -f "${TMP_FOLDER}/docif_tests.cid"
 
-# Wait for push to finish if possible
-if [ -r "${TMP_FOLDER}/push_baseimage.lock" ]; then
-	echo "[INFO] Waiting for the baseimage push to complete."
-
-	set +e
-
-	COUNTER=0
-	TIMEOUT=600
-	# Wait for the push to baseimage to finish
-	if [ -f ${TMP_FOLDER}/push_baseimage.lock ]; then
-		while [ -z "$(cat ${TMP_FOLDER}/push_baseimage.lock)" ]; do
-			sleep 1
-			COUNTER="$(expr "$COUNTER" + 1)"
-			if [ "$COUNTER" -gt "$TIMEOUT" ]; then
-				# Break out of loop by forcing a failure
-				echo "[ERR] Push command timed out!" >&2
-				echo "1" > "${TMP_FOLDER}/push_baseimage.lock"
-			fi
-		done
-	fi
-
-	if [ "$(cat ${TMP_FOLDER}/push_baseimage.lock)" -ne 0 ]; then
-		echo "[WARN] The push to the source repository FAILED. This usually means the dockerhub is down." >&2
-	fi
-	set -e
-
-	rm -f ${TMP_FOLDER}/push_baseimage.lock
+if [ $EXIT -ne 0 ]; then
+	# Always pass this test (the other te
+	exit "$EXIT"
 fi
+
