f9e42268b42d95f2b5e6e48d69b2e8a372464294
Jay Kamat <jaygkamat@gmail.com>
2016-11-13 16:05:16 -0500

    Clean up debug information for CACHING_SHA

--- commands/buildbaseimage.sh
+++ commands/buildbaseimage.sh
@@ -5,14 +5,18 @@ DIR=$(cd $(dirname $0) ; pwd -P)
 source ${DIR}/../util/docker_common.sh
 
 
-if [ -n "$CACHING_SHA" ] && curl -s https://registry.hub.docker.com/v1/repositories/${BASEIMAGE_REPO}/tags/${CACHING_SHA} \
+if [ -n "${CACHING_SHA:-}" ] && curl -s https://registry.hub.docker.com/v1/repositories/${BASEIMAGE_REPO}/tags/${CACHING_SHA} \
 	-o /dev/null -w "%{http_code}" | fgrep -q '200'; then
 	echo "[INFO] Cached baseimage found, pulling from ${BASEIMAGE_REPO}:${CACHING_SHA}"
 	# The tag currently exists, and can be pulled
 	docker pull ${BASEIMAGE_REPO}:${CACHING_SHA}
 else
 	# The tag does not exist, let's build it!
-	echo "[INFO] Cached baseimage of sha ${CACHING_SHA} not found. Building a new image."
+	if [ -z "${CACHING_SHA:-}" ]; then
+		echo "[INFO] Caching sha not found. Make sure you have SHA files for us to SHA."
+	else
+		echo "[INFO] Cached baseimage ${BASEIMAGE_REPO}:${CACHING_SHA} not found. Building a new image."
+	fi
 
 	set +e
 
